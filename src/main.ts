0bfa90c786807abd3eae3e3b22eab6329cef42c9
hwkd <justinkimkorea@gmail.com>
2024-08-23 22:50:41 +1000

    [main] add basic benchmark

--- src/main.ts
+++ src/main.ts
@@ -26,16 +26,41 @@ function toJS(ast: Node): unknown {
   }
 }
 
+// Performance test
+function perf(name: string, fn: () => any) {
+  let totalTime = 0;
+  const iterations = 10000;
+
+  for (let i = 0; i < iterations; i++) {
+    const start = performance.now();
+    fn();
+    const end = performance.now();
+    totalTime += end - start;
+  }
+
+  console.log(`${[name]}: Average execution time: ${totalTime / iterations} ms`);
+}
+
 function main() {
   const jsonFile = fs.readFileSync("package.json", "utf-8");
   const parser = newParser(jsonFile);
   const ast = parser.parse();
 
-  // Print AST
+  //Print AST
   console.log(JSON.stringify(ast, null, 2));
 
-  // Print JavaScript object
+  //Print JavaScript object
   console.log(toJS(ast));
+
+  perf("Handwritten", () => {
+    const parser = newParser(jsonFile);
+    const ast = parser.parse();
+    toJS(ast);
+  })
+
+  perf("JSON.parse", () => {
+    JSON.parse(jsonFile);
+  })
 }
 
 main();
